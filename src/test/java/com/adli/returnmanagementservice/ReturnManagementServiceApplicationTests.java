e0c251fdccd498758e7546a91135f4c4f1f87fb5
adli@mamiteam.com <adli@mamiteam.com>
2022-07-24 16:54:45 +0700

    Init

---
+++ src/test/java/com/adli/returnmanagementservice/ReturnManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adli.returnmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReturnManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
